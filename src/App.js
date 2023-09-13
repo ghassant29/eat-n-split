40323e44fd3519a9cabb718896f3ecec54a30e0d
ghassant29 <gass304@gmail.com>
2023-09-13 17:50:47 +0300

    Adding Forms Components

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ const initialFriends = [
 export default function App() {
   return (
    <div className='app'><div className='sidebar'>
-    <FriendsList /></div></div>
+    <FriendsList />
+    <FormAddFriend />
+    <Button>Add friend</Button></div>
+    <FormSplitBill /></div>
    
   );
 }
@@ -38,8 +41,35 @@ function Friend({friend}){
   {friend.balance<0 && <p className='red'>You owe {friend.name} {Math.abs(friend.balance)}$</p>}
   {friend.balance>0 && <p className='green'>{friend.name} owes you  {Math.abs(friend.balance)}$</p>}
   {friend.balance===0 && <p className='red'>You and your {friend.name} are even</p>}
-  <button className='button'>Select</button>
+  <Button>Select</Button>
   </li>
 }
-
+function Button({children}){
+return <button className='button'>{children}</button>
+}
+function FormAddFriend(){
+  return <form className='form-add-friend'>
+    <label>🧑‍🤝‍🧑Friend name</label>
+    <input type='text'/>
+    <label>🌄Image URL</label>
+    <Button>Add</Button>
+  </form>
+}
+function FormSplitBill(){
+  return <form className='form-split-bill'>
+    <h2>Split a bill with X</h2>
+    <label>Bill value 💲</label>
+    <input type='text'/>
+    <label>🧍Your expense</label>
+    <input type='text'/>
+    <label>🧑‍🤝‍🧑X's expense 💲</label>
+    <input type='text' disabled/>
+    <label>💰Who is paying the bill</label>
+    <select> <option value="user">You</option>
+    <option value="friend">X</option></select>
+   
+    <Button>Split Bill</Button>
+    
+  </form>
+}
 
